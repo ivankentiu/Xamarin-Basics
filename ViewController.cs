faacd29f1862a028c4d6dd7c4c50b34d8a8ccdf9
ivantiu <ivankentiu@gmail.com>
2017-12-26 11:14:07 +0800

    Fix

--- ViewController.cs
+++ ViewController.cs
@@ -6,13 +6,11 @@ namespace MyFirstApp
 {
     public partial class ViewController : UIViewController
     {
-        protected ViewController(IntPtr handle) : base(handle)
-        {
+        protected ViewController(IntPtr handle) : base(handle) {
             // Note: this .ctor should not contain any initialization logic.
         }
 
-        public override void ViewDidLoad()
-        {
+        public override void ViewDidLoad() {
             base.ViewDidLoad();
 
             textField.KeyboardType = UIKeyboardType.NumberPad;
@@ -22,22 +20,19 @@ namespace MyFirstApp
             textSizeSlider.ValueChanged += TextSizeSlider_ValueChanged;
         }
 
-        void TextSizeSlider_ValueChanged(object sender, EventArgs e)
-        {
+        void TextSizeSlider_ValueChanged(object sender, EventArgs e) {
             var value = textSizeSlider.Value;
             textView.Font = UIFont.SystemFontOfSize(value);
         }
 
-        void ColorSwitch_ValueChanged(object sender, EventArgs e)
-        {
+        void ColorSwitch_ValueChanged(object sender, EventArgs e) {
             if (colorSwitch.On)
                 switchLabel.Text = "De-activate Color";
             else
                 switchLabel.Text = "Activate Color";
         }
 
-        void Button_TouchUpInside(object sender, EventArgs e)
-        {
+        void Button_TouchUpInside(object sender, EventArgs e) {
             textField.ResignFirstResponder();
 
             transportText();
@@ -46,10 +41,9 @@ namespace MyFirstApp
 
         }
 
-        void changeBackgroundColor()
-        {
-            if (colorSwitch.On)
-            {
+        void changeBackgroundColor() {
+            if (colorSwitch.On) {
+
                 var randSeed = new Random();
                 var rand1 = (float)randSeed.Next(50, 100);
                 var rand2 = (float)randSeed.Next(50, 100);
@@ -60,16 +54,14 @@ namespace MyFirstApp
             }
         }
 
-        private async void transportText()
-        {
+        private async void transportText() {
             textView.Text = "";
 
-            while (textField.Text != "")
-            {
-                await Task.Factory.StartNew(() =>
-                {
-                    InvokeOnMainThread(() =>
-                    {
+            while (textField.Text != "") {
+                await Task.Factory.StartNew(() => {
+
+                    InvokeOnMainThread(() => {
+
                         var letter = textField.Text.Substring(0, 1);
                         textView.Text = textView.Text + letter;
                         textField.Text = textField.Text.Substring(1);
@@ -82,30 +74,25 @@ namespace MyFirstApp
             }
         }
 
-        private void updateProgressView()
-        {
+        private void updateProgressView() {
             var textLength = (float)(textField.Text.Length + textView.Text.Length); // ratio is better in float or double
             var transferProgress = textView.Text.Length / textLength; // int / float = float
             progressView.Progress = transferProgress;
         }
 
-        public override void ViewWillAppear(bool animated)
-        {
+        public override void ViewWillAppear(bool animated) {
             base.ViewWillAppear(animated);
         }
 
-        public override void ViewDidDisappear(bool animated)
-        {
+        public override void ViewDidDisappear(bool animated) {
             base.ViewDidDisappear(animated);
         }
 
-        public override void ViewWillDisappear(bool animated)
-        {
+        public override void ViewWillDisappear(bool animated) {
             base.ViewWillDisappear(animated);
         }
 
-        public override void DidReceiveMemoryWarning()
-        {
+        public override void DidReceiveMemoryWarning() {
             base.DidReceiveMemoryWarning();
             // Release any cached data, images, etc that aren't in use.
         }
